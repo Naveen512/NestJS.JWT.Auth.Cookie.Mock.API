d0bf503b86c3dd391cbe3b8fa5e7f2e1cdaaa9d0
hchowdary <hchowdary@qualminds.com>
2021-04-24 05:38:48 +0530

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    allowedHeaders:"*"
+  });
   app.use(cookieParser())
   await app.listen(3000);
 }
